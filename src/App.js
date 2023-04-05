9aeab956b064ce8ae031df7d9e06be007ef698eb
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-04-05 13:04:08 +0600

    Add library react-router-dom and use this program

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Hello World</h1>
+      <Link to="/">Home</Link>
+      <Link to="MyPage">My Pge</Link>
+
+      <Routes>
+        <Route path="/" element={<h2>Hello World</h2>} />
+        <Route path="MyPage" element={<h2>Bye World</h2>} />
+      </Routes>
     </div>
   );
 }
