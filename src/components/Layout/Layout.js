6b8dd0a519b911857ff15c34f50aecdac04eb10e
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-04-17 14:55:02 +0600

    add and import CartLink

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import Nav from "./Nav/Nav";
 import "./Layout.css";
 import Logo from "../Logo/Logo";
 import CategoryList from "../CategoryList/CategoryList";
+import CartLink from "../CartLink/CartLink";
 export default function Layout({children}) {
   return (
     <div className="Layout">
       <header>
         <Logo/>
         <Nav/>
+        <CartLink/>
       </header>
       <aside>
         <CategoryList/>
