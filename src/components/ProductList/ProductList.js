a8f9f099220288778af846158a2e187b1cf3cd22
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-04-17 14:33:49 +0600

    add to cart in product list

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import "./ProductList.css";
 import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
+import AddToCart from "../AddToCart/AddToCart";
 
 export default function  ProductList({category}) {
   const {products} = useContext(AppContext);
@@ -14,6 +15,7 @@ export default function  ProductList({category}) {
         {product.name}
       </NavLink>
       <span>{product.price} $</span>
+      <AddToCart product={product} />
     </div>
   ));
 
