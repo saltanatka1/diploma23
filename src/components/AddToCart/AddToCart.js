c0813e7c9fa1f2e940e4cf3162ee29b56984b9f2
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-04-17 14:32:53 +0600

    add cart and add to counter in cart

---
+++ src/components/AddToCart/AddToCart.js
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import "./AddToCart.css";
+import { AppContext } from "../../App";
+export default function AddToCart({ product }) {
+  const { cart, setCart } = useContext(AppContext);
+
+  function onAddToCartClick() {
+    const qty =cart[product.id] ? cart[product.id]+ 1 : 1;
+    setCart({
+      ...cart, //положить текущее соержание корзинки
+      [product.id]:qty, //добавить текущий товар
+    });
+  }
+  return (
+    <div className="AddToCart">
+      {cart[product.id] ? cart[product.id] : 0}
+      <button onClick={onAddToCartClick}>Add to cart</button>
+    </div>
+  );
+}
