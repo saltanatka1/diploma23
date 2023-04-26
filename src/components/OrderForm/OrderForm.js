8ec44fe7532da7633a053855b2b70007a424f166
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-04-26 13:59:31 +0600

    add access only to the customer and admin

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -6,28 +6,28 @@ import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function OrderForm() {
-  const { cart, setCart } = useContext(AppContext);
+  const { cart, setCart, user } = useContext(AppContext);
   const navigate = useNavigate();
 
-  if (Object.keys(cart).length===0){
+  if (Object.keys(cart).length === 0) {
     return "Your cart is empty.";
   }
+  if (!user) {
+    return "Please login  to create an order";
+  }
   function onFormSubmit(event) {
-
     event.preventDefault();
-
     const formData = new FormData(event.target);
     addDoc(ordersCollection, {
       name: formData.get("name"),
       phone: formData.get("phone"),
-      email: formData.get("email"),
+      user: user.uid,
       address: formData.get("address"),
       cart: cart,
-    })
-    .then (doc => {
+    }).then((doc) => {
       setCart({});
-      navigate('/thank-you');
-    })
+      navigate("/thank-you");
+    });
   }
 
   return (
@@ -38,9 +38,6 @@ export default function OrderForm() {
       </label>
       <label>
         Phone : <input type="tel" name="phone" required />
-      </label>
-      <label>
-        Email : <input type="email" name="email" required />
       </label>
       <label>
         Address : <input type="text" name="address" required />
