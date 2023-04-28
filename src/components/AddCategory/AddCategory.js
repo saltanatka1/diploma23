f6db217dd8c46986e5d679d8370925190d4b4dd8
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-04-28 14:40:32 +0600

    add category to firestore

--- src/components/AddCategory/AddCategory.js
+++ src/components/AddCategory/AddCategory.js
@@ -1,29 +1,41 @@
+import { addDoc } from "firebase/firestore/lite";
 import { useState } from "react";
+import { categoryCollection } from "../../firebase";
 
 function AddCategory() {
-  const [category, setCategory] = useState("")
+  const [category, setCategory] = useState("");
 
-  function onChangeCategory(event){
-    return ( 
-      setCategory(event.target.value)
-    );
+  function onChangeCategory(event) {
+    return setCategory(event.target.value);
   }
 
   function onAddCategory(params) {
-    
+    const name = category.trim();
+
+    if (name.length <= 5) {
+      alert(
+        "Please provide a category name with minimum length of 5 characters"
+      );
+    }
+    addDoc(categoryCollection, {
+      name: category.trim(),
+      slug: category.trim().replace(" ", "-").toLocaleLowerCase(),
+    }).then(() => {
+      setCategory("");
+    });
   }
-  
-  
+
   return (
-  <div className="AddCategory"> 
-    <input 
-    size="15"
-    type="text" 
-    value={category}
-    placeholder="Category name " 
-    onchange={onChangeCategory}/>
-    <button onClick={onAddCategory}>+</button>
-  </div>
+    <div className="AddCategory">
+      <input
+        size="15"
+        type="text"
+        value={category}
+        placeholder="Category name "
+        onchange={onChangeCategory}
+      />
+      <button onClick={onAddCategory}>+</button>
+    </div>
   );
 }
 
