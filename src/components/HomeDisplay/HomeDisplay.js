d65c8ffd7cb88746dd9a058378fd0cfa5a85524c
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-03 13:45:50 +0600

    create component for home page

---
+++ src/components/HomeDisplay/HomeDisplay.js
@@ -0,0 +1,37 @@
+function HomeDisplay() {
+  return (
+    <div className="HomeDisplay">
+      <section>
+        <h1>SAIMA etno-shop</h1>
+        <p>
+          Welcome to SAIMA, your one-stop online destination for exquisite
+          Kyrgyz-style clothing and decorations. Immerse yourself in the rich
+          cultural heritage of Kyrgyzstan as you explore our extensive
+          collection of traditional garments and unique decor items.
+        </p>
+      </section>
+      <section>       
+      <h1>Kyrgyz style </h1>
+        <img src="https://fsd.multiurok.ru/html/2020/02/07/s_5e3d722a394d9/1345266_4.jpeg" alt="kyrgyz clothes" />
+        <img src="" alt="kyrgyz clothes" />
+        <img src="" alt="kyrgyz clothes" />
+        </section>
+
+        <section>
+          <h1>SAIMA in Kyrgyz culture</h1>
+          <p>
+            At SAIMA, we pride ourselves on offering a wide range of products
+            that showcase the beauty and craftsmanship of Kyrgyz culture.
+            Whether you're looking for a stylish outfit for a special occasion
+            or searching for authentic decorations to add a touch of elegance to
+            your home, our etno-shop has something to suit every taste and
+            preference.
+          </p>
+        </section>
+  
+
+    </div>
+  );
+}
+
+export default HomeDisplay;
