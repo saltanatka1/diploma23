2ba1aa0b41b3f4f616f172c848b47d623a816564
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-03 13:44:08 +0600

    add navtoggle and drawer in layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,26 @@
-import Nav from "./Nav/Nav";
+import Nav from "../Nav/Nav";
 import "./Layout.css";
 import Logo from "../Logo/Logo";
 import CategoryList from "../CategoryList/CategoryList";
 import CartLink from "../CartLink/CartLink";
 import Auth from "../Auth/Auth";
+import Footer from "../../pages/Footer";
+import NavToggle from "../NavToggle/NavToggle";
+import Drawer from "../Drawer/Drawer";
+import { useState } from "react";
 export default function Layout({children}) {
+    const [drawerOpen, setDrawerOpen] = useState(false);
+  
+    function toggleDrawer() {
+      setDrawerOpen(!drawerOpen);
+    }
   return (
     <div className="Layout">
       <header>
         <Logo/>
         <Nav/>
+        <NavToggle callback={toggleDrawer} />
+        <Drawer open={drawerOpen} toggle={toggleDrawer} />
         <CartLink/>
         <Auth/>
       </header>
@@ -17,7 +28,7 @@ export default function Layout({children}) {
         <CategoryList/>
       </aside>
       <main>{children}</main>
-      <footer>footer</footer>
+      <footer><Footer/></footer>
     </div>
   );
 }
