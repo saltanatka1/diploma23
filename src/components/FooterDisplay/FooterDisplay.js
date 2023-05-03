2c163317491631936598c0d8b527d0330f85c44c
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-03 13:43:04 +0600

    create component for footer

---
+++ src/components/FooterDisplay/FooterDisplay.js
@@ -0,0 +1,14 @@
+import Logo from "../Logo/Logo";
+import "./FooterDisplay.css";
+
+function FooterDisplay() {
+  return ( 
+    <div className="FooterDisplay">
+      <Logo/>
+      <h3>About SAIMA</h3>
+      <h3>More from SAIMA</h3>
+    </div>
+  );
+}
+
+export default FooterDisplay;
---
+++ src/pages/Footer.js
@@ -0,0 +1,11 @@
+import FooterDisplay from "../components/FooterDisplay/FooterDisplay";
+
+function Footer() {
+  return ( 
+    <div className="Footer">
+      <FooterDisplay/>
+    </div>
+  );
+}
+
+export default Footer;
