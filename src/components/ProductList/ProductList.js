4d98fede58edf1258541f53f7b43dd8c3ecd7b6c
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-12 01:51:59 +0600

    add container and content set className

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -6,27 +6,40 @@ import AddToCart from "../AddToCart/AddToCart";
 import AddProduct from "../AddProduct/AddProduct";
 import DeleteProduct from "../DeleteProduct/DeleteProduct";
 
-export default function  ProductList({category}) {
-  const {products} = useContext(AppContext);
+export default function ProductList({ category }) {
+  const { products } = useContext(AppContext);
 
-  const output = products.filter(product => product.category===category.id)
-  .map(product => (
-    <div key={product.id} className="productItem"> 
-      <img src={product.picture} alt={product.name}/>
-      <NavLink to={'/products/' + product.slug}>
-        {product.name}
-      </NavLink>
-      <span>{product.price} $</span>
-      <AddToCart product={product} />
-      <DeleteProduct product={product}/>
-    </div>
-  ));
+  const output = products
+    .filter((product) => product.category === category.id)
+    .map((product) => (
+      <div key={product.id} className="productItem">
+        <div className="container">
+          <div className="card">
+            <div className="imgBx">
+              <img src={product.picture} alt={product.name} />
+            </div>
+            <div className="contentBx">
+              <h2>{product.name}</h2>
+              <NavLink to={"/products/" + product.slug} className="link">
+                {product.name}
+              </NavLink>
+              <div className="price">
+                <span>{product.price} $</span>
+                <AddToCart product={product} className="add" />
+              </div>
+            </div>
+          </div>
+          <div />
+        </div>
+
+        <DeleteProduct product={product} />
+      </div>
+    ));
 
   return (
     <div className="ProductList">
       {output}
-      <AddProduct category={category}/>
-      
+      <AddProduct category={category} />
     </div>
   );
 }
