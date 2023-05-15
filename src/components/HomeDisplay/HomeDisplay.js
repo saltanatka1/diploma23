cf79aa7dcb40f5892cc528625552e10fb3fceeb6
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-15 14:41:05 +0600

    delete div with named item

--- src/components/HomeDisplay/HomeDisplay.js
+++ src/components/HomeDisplay/HomeDisplay.js
@@ -13,54 +13,43 @@ function HomeDisplay() {
             collection of traditional garments and unique decor items.
           </p>
         </div>
-        <div className="blank"> </div>
-        <div className="content second">
-          <h1>Kyrgyz style </h1>
-          <div className="items">
-            <div className="item">
-              <img
-                className="img img-first"
-                src="https://i.pinimg.com/originals/c6/77/82/c67782dd8034f501fc5f1145674b1042.jpg"
-                alt="kyrgyz girl"
-              />
-            </div>
-            <div className="item">
-              <img
-                className="img img-second"
-                src="https://i.pinimg.com/originals/d4/42/bd/d442bda392008e59439664ce7a75057d.jpg"
-                alt="kazakh girl"
-              />
-            </div>
-            <div className="item">
-              <img
-                className="img img-third"
-                src="https://i.pinimg.com/originals/86/17/aa/8617aad6ca0b2ddb49b4af9fd8ffed2e.jpg"
-                alt="beatiful girl"
-              />
-            </div>
-            <div className="item">
-              <img
-                className="img img-third"
-                src="https://i.pinimg.com/originals/fb/f3/4b/fbf34b4cc190fa9bc8e657cdbba0d5a0.jpg"
-                alt="girl"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="blank"></div>
-        <div className="content third">
-          <h1>SAIMA in Kyrgyz culture</h1>
-          <p>
-            At SAIMA, we pride ourselves on offering a wide range of products
-            that showcase the beauty and craftsmanship of Kyrgyz culture.
-            Whether you're looking for a stylish outfit for a special occasion
-            or searching for authentic decorations to add a touch of elegance to
-            your home, our etno-shop has something to suit every taste and
-            preference.
-          </p>
-        </div>
-        <div className="blank"></div>
       </div>
+
+      <div className="blank"> </div>
+      <div className="content second">
+        <h1>Kyrgyz style </h1>
+        <div className="items">
+          <img
+            className="img img-first"
+            src="https://i.pinimg.com/originals/c6/77/82/c67782dd8034f501fc5f1145674b1042.jpg"
+            alt="kyrgyz girl"
+          />
+
+          <img
+            className="img img-second"
+            src="https://i.pinimg.com/originals/d4/42/bd/d442bda392008e59439664ce7a75057d.jpg"
+            alt="kazakh girl"
+          />
+
+          <img
+            className="img img-third"
+            src="https://i.pinimg.com/originals/86/17/aa/8617aad6ca0b2ddb49b4af9fd8ffed2e.jpg"
+            alt="beatiful girl"
+          />
+        </div>
+      </div>
+      <div className="blank"></div>
+      <div className="content third">
+        <h1>SAIMA in Kyrgyz culture</h1>
+        <p>
+          At SAIMA, we pride ourselves on offering a wide range of products that
+          showcase the beauty and craftsmanship of Kyrgyz culture. Whether
+          you're looking for a stylish outfit for a special occasion or
+          searching for authentic decorations to add a touch of elegance to your
+          home, our etno-shop has something to suit every taste and preference.
+        </p>
+      </div>
+      <div className="blank"></div>
     </div>
   );
 }
