7e8b955ae16f2e443cc8aa278e040b93102946b5
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-15 14:38:56 +0600

    change add category component

--- src/components/AddCategory/AddCategory.js
+++ src/components/AddCategory/AddCategory.js
@@ -1,14 +1,14 @@
-import React, { useContext, useState } from "react";
-import "./AddCategory.css";
 import { addDoc } from "firebase/firestore";
+import { useContext, useState } from "react";
 import { categoryCollection } from "../../firebase";
 import { AppContext } from "../../App";
 
-export default function AddCategory() {
-  const {user} = useContext(AppContext);
+const AddCategory = () => {
+  const { user } = useContext(AppContext);
   const [category, setCategory] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  if (!user || !user.isAdmin){
+  if (!user || !user.isAdmin) {
     return null;
   }
 
@@ -18,29 +18,43 @@ export default function AddCategory() {
 
   function onAddCategory() {
     const name = category.trim();
-    if (name.length <= 5) {
+
+    if (name.length < 5) {
       alert(
-        "Plase provide a category name with minimum length of 5 characters"
+        "Please provide a category name with minimum length of 5 characters."
       );
+
       return;
     }
+
+    setIsSubmitting(true);
+
     addDoc(categoryCollection, {
       name: name,
-      slug: category.trim().replaceAll(" ", "-").toLocaleLowerCase(),
-    }).then(() => {
-      setCategory("");
-    });
+      slug: name.replaceAll(" ", "-").toLocaleLowerCase(),
+    })
+      .then(() => {
+        setCategory("");
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
   }
+
   return (
     <div className="AddCategory">
       <input
         size="15"
         type="text"
         value={category}
-        placeholder="Category Name"
+        placeholder="Category name"
         onChange={onChangeCategory}
       />
-      <button onClick={onAddCategory}>+</button>
+      <button onClick={onAddCategory} disabled={isSubmitting}>
+        +
+      </button>
     </div>
   );
-}
+};
+
+export default AddCategory;
