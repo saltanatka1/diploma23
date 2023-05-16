7ab19e3c3f428bbfd9f64302a4dfbaddf9ab3670
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-16 14:52:14 +0600

    add class name and div class name

--- src/components/FooterDisplay/FooterDisplay.js
+++ src/components/FooterDisplay/FooterDisplay.js
@@ -1,12 +1,21 @@
 import Logo from "../Logo/Logo";
+import Nav from "../Nav/Nav";
 import "./FooterDisplay.css";
 
 function FooterDisplay() {
-  return ( 
+  return (
     <div className="FooterDisplay">
-      <Logo/>
-      <h3>About SAIMA</h3>
-      <h3>More from SAIMA</h3>
+      <Logo />
+      <div className="menu">
+        <h3>Menu SAIMA</h3>
+        <Nav className="nav" />
+      </div>
+      <div className="more">
+        <h3>More from SAIMA</h3>
+      </div>
+      <div className="contacts">
+        <h3>Contacts</h3>
+      </div>
     </div>
   );
 }
