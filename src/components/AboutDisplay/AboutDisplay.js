76ac3d29957c80d9f9710e4adc44f44af8d1569c
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-17 14:31:17 +0600

    add about page and description

---
+++ src/components/AboutDisplay/AboutDisplay.js
@@ -0,0 +1,142 @@
+import "./AboutDisplay.css";
+export default function AboutDisplay() {
+  return(
+  <div className="AboutDisplay">
+    <div className="container">
+      <div id="parallax-world-of-ugg">
+        <section>
+          <div class="title">
+            <h3>Let's do some</h3>
+            <h1>About SAIMA</h1>
+          </div>
+        </section>
+
+        <section>
+          <div class="parallax-one">
+            <h2>KYRGYZ STYLE</h2>
+          </div>
+        </section>
+
+        <section>
+          <div class="block">
+            <p>
+              <span class="first-character sc">Kyrgyz style </span>encompasses
+              the traditional clothing, art, music, and lifestyle of the Kyrgyz
+              people, who are a Turkic ethnic group primarily residing in
+              Kyrgyzstan, a landlocked country in Central Asia. Kyrgyz culture
+              is rich and diverse, with a unique blend of nomadic traditions and
+              influences from neighboring regions. One of the most distinctive
+              aspects of Kyrgyz style is their traditional clothing. The
+              traditional attire reflects the nomadic heritage of the Kyrgyz
+              people and is often adorned with intricate patterns and vibrant
+              colors. The main garment for men is the "chapan," a long robe-like
+              coat made of wool or felt. It features wide sleeves and is usually
+              embellished with decorative embroidery. The chapan serves as both
+              a practical garment to protect against the harsh mountain climate
+              and a symbol of cultural identity.
+            </p>
+            <p class="line-break margin-top-10"></p>
+            <p class="margin-top-10">
+              For women, the traditional Kyrgyz clothing includes a dress called
+              a "beldemchi" or "chyptama." It is a long, loose-fitting dress
+              with a high waistline and wide sleeves. The dress is often made
+              from silk or cotton and is adorned with colorful patterns and
+              embroidery. Women also wear a headdress called a "elechek" or
+              "kalpak," which is a tall, cone-shaped hat decorated with beads,
+              feathers, and other ornaments. In addition to clothing, Kyrgyz
+              style is also evident in their art and craftsmanship. Kyrgyz
+              artisans are known for their skill in creating intricate and
+              colorful tapestries, known as "shyrdaks." These felt rugs are made
+              by hand using traditional techniques and feature geometric
+              patterns and symbols inspired by nature and Kyrgyz folklore.
+            </p>
+          </div>
+        </section>
+
+        <section>
+          <div class="parallax-two">
+            <h2>KAZAKH STYLE</h2>
+          </div>
+        </section>
+
+        <section>
+          <div class="block">
+            <p>
+              <span class="first-character ny">Kazakh style</span>
+              refers to the traditional clothing, customs, and aesthetics of the
+              Kazakh people, who are a Turkic ethnic group predominantly
+              residing in Kazakhstan, as well as in neighboring countries such
+              as China, Mongolia, and Russia. The Kazakh culture has deep roots
+              in nomadic traditions and is characterized by a distinctive style
+              of clothing that reflects their heritage. One of the most iconic
+              elements of Kazakh traditional clothing is the "chapan" or
+              "shapan." It is a long robe-like coat made of wool or fur and is
+              commonly worn by both men and women. The chapan features a high
+              collar, wide sleeves, and is often adorned with intricate
+              embroidery and patterns. The coat serves as protection against the
+              harsh climate of the Central Asian steppes and also serves as a
+              symbol of cultural identity.
+            </p>
+            <p class="line-break margin-top-10"></p>
+            <p class="margin-top-10">
+              Kazakh women traditionally wear a dress called a "saukele" for
+              special occasions and weddings. The saukele is a headdress made of
+              felt or velvet, embellished with gold or silver embroidery, beads,
+              and precious stones. It is often paired with a long, flowing dress
+              known as a "koye" or "koynek," which is typically made of silk or
+              brocade fabric and features vibrant colors and ornate patterns.
+              Another notable aspect of Kazakh traditional clothing is the
+              "telpek," a traditional hat made of sheepskin or fox fur. The
+              telpek is a cylindrical hat with a flat crown and is worn by both
+              men and women, especially during winter to provide warmth. The hat
+              is often decorated with fur tassels and embroidery, showcasing the
+              craftsmanship and artistic skills of the Kazakh people.
+            </p>
+          </div>
+        </section>
+
+        <section>
+          <div class="parallax-three">
+            <h2>TURKISH STYLE</h2>
+          </div>
+        </section>
+
+        <section>
+          <div class="block">
+            <p>
+              <span class="first-character atw">Turkish style</span>
+              encompasses a wide range of influences, from traditional Ottoman
+              attire to contemporary fashion trends. Turkey, located at the
+              crossroads of Europe and Asia, has a rich cultural heritage that
+              is reflected in its clothing and fashion. Traditional Turkish
+              clothing is heavily influenced by the historical Ottoman Empire.
+              One iconic garment is the "kaftan," a long, flowing robe-like
+              dress worn by both men and women. Kaftans were made from luxurious
+              fabrics such as silk and velvet and often featured elaborate
+              embroidery and decorative patterns. They were worn on special
+              occasions and by the ruling class. Over time, the kaftan has
+              evolved into modern variations and is still appreciated for its
+              elegant and regal aesthetic.
+            </p>
+            <p class="line-break margin-top-10"></p>
+            <p class="margin-top-10">
+              For men, traditional Turkish clothing includes the "şalvar" (baggy
+              trousers) and "cepken" (a long jacket or vest). These garments
+              were popular during the Ottoman era and are still worn on
+              ceremonial occasions, particularly in rural areas. The "fes" is
+              another traditional Turkish accessory—a cylindrical red hat made
+              of felt, often associated with Ottoman officials and scholars. In
+              rural regions of Turkey, traditional clothing varies among
+              different ethnic groups and communities. For example, in the
+              southeastern Anatolian region, Kurdish and Arab influences are
+              evident in the clothing style. Traditional Kurdish attire includes
+              the "şalvar" (baggy trousers) and the "çuval" (a large, loose
+              shirt), often complemented by colorful waistbands and scarves.
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+  );
+}
