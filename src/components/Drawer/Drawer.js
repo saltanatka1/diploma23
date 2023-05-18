ff3e89c25fc4ff9b5ae8f76e596cbe777cb722b9
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-19 00:37:57 +0600

    add logotip to side bar navigation

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,6 +1,7 @@
 
 import Nav from "../Nav/Nav";
 import "./Drawer.css"
+import Logo from "../Logo/Logo";
 
 function Drawer({ open, toggle }) {
   const drawerClassNames = `Drawer ${open ? "open" : ""}`;
@@ -8,6 +9,7 @@ function Drawer({ open, toggle }) {
     <div className={drawerClassNames}>
       <div onClick={toggle} className="backdrop"></div>
       <div className="content">
+        <Logo/>
         <Nav />
       </div>
     </div>
