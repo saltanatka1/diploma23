d5074f0e56d1d3501cdbd38df06529e2e122784d
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-19 00:41:01 +0600

    add deliverydisplay component to delivery page and remove title

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -1,8 +1,9 @@
+import DeliveryDisplay from "../components/DeliveryDisplay/DeliveryDisplay";
+
 export default function Delivery (){
   return(
     <div className="Delivery">
-      <h1>Delivery</h1>
-      <p>This is the delivery page</p>
+      <DeliveryDisplay/>
     </div>
   );
 }
