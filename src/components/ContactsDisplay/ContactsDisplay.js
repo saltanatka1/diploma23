8e783a30d562a22890a79ee34bbabff32a690a2a
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-19 02:16:15 +0600

    add content to contact page

--- src/components/ContactsDisplay/ContactsDisplay.js
+++ src/components/ContactsDisplay/ContactsDisplay.js
@@ -1,9 +1,42 @@
+import { Link } from "react-router-dom";
 import "./ContactsDisplay.css";
 
-export default function ContactsDisplay (){
+export default function ContactsDisplay() {
   return (
     <div className="ContactsDisplay">
-      
+      <div className="container">
+        <div className="content">
+          <div className="contact">
+            <h1>Contact Us</h1>
+            <p>
+              A Contact Us page is essential for your site to engage with
+              visitors and customers. Check out these helpful tips and contact
+              us page examples.
+            </p>
+            <Link href="tel:+996708813744">+996708813744</Link>
+            <Link href="email:egizbaevasaltanat05@gmail.com">
+              egizbaevasaltanat05@gmail.com
+            </Link>
+          </div>
+          <div className="image-contact">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/002/147/279/original/young-man-and-woman-with-headphones-microphone-and-computer-customer-service-support-or-call-center-concept-free-vector.jpg"
+              alt="contact"
+            />
+          </div>
+        </div>
+        <div className="content location">
+          <h1>Our location</h1>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d5883.727000129256!2d78.4082362!3d42.4944545!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1684438410463!5m2!1sru!2skg"
+            className="map"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            title="map"
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 }
