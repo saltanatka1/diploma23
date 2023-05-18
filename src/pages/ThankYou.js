1dd977ed6811b8051377024cb155315c9e6c114e
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-19 02:18:56 +0600

    set time out to thank you page

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -1,8 +1,14 @@
-export default function ThankYou (){
+export default function ThankYou() {
+  setTimeout(() => {
+    window.location.href = "/";
+  }, 2000);
   return (
     <div className="ThankYou">
-    <h1>Thank you!</h1>
-    <p>Your order has been received. Our specialists will contact with you shortly to confirm it.</p>
-  </div>
-  )
+      <h1>Thank you!</h1>
+      <p>
+        Your order has been received. Our specialists will contact with you
+        shortly to confirm it.
+      </p>
+    </div>
+  );
 }
