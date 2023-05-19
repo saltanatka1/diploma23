598fafac0209bfef39821e0ae1367faeceabc82b
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-19 11:53:59 +0600

    change thankyou page content

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -1,14 +1,17 @@
+import "./ThankYou.css";
 export default function ThankYou() {
-  setTimeout(() => {
-    window.location.href = "/";
-  }, 2000);
+  // setTimeout(() => {
+  //   window.location.href = "/";
+  // }, 2000);
   return (
     <div className="ThankYou">
-      <h1>Thank you!</h1>
-      <p>
-        Your order has been received. Our specialists will contact with you
-        shortly to confirm it.
-      </p>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h3 class="animate-charcter">Thank You!</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
