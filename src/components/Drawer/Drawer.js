3fbae776af5204b8f87f66d805d85213e88c65ec
Saltanat Egizbaeva <egizbaevasaltanat05@gmail.com>
2023-05-24 15:26:52 +0600

    add function to drawer

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,15 +1,21 @@
-
 import Nav from "../Nav/Nav";
-import "./Drawer.css"
+import "./Drawer.css";
 import Logo from "../Logo/Logo";
 
 function Drawer({ open, toggle }) {
+  let menuBtn = document.querySelectorAll(".NavItem a");
+  let menu = document.querySelector(".Drawer");
+  menuBtn.forEach((el) => {
+    el.addEventListener("click", function () {
+      menu.classList.remove("open");
+    });
+  });
   const drawerClassNames = `Drawer ${open ? "open" : ""}`;
   return (
     <div className={drawerClassNames}>
       <div onClick={toggle} className="backdrop"></div>
       <div className="content">
-        <Logo/>
+        <Logo />
         <Nav />
       </div>
     </div>
